ec1ce9e5b0a25dd9eb8d692dbe9552e1598f0104
koontz <koooontz@yahoo.com>
2015-11-03 18:24:11 -0500

    Boom

--- search.py
+++ search.py
@@ -1,3 +1,7 @@
+import document.py
+
+
+
 def main():
 	query = ""
 	while query != "quit":
@@ -11,8 +15,23 @@ def main():
 		print "\n"
 	
 def score_documents(query):
-	return []
+	return bm25Score(query)
 
+def bm25Score(query):
+	output = []
+	k = 1
+	b= 0.75
+	documents = document.getDocuments()
+	for doc in documents:
+		score = 0
+		for searchTerm in query:
+			IDF = Document.IDF(searchTerm)
+			top = doc.freq(searchTerm)*(k+1)
+			bot = doc.freq(searchTerm)+k*(1-b+b*doc.length()/document.avgdl())
+			score+= IDF * top / bot
+		output.append((doc.filename,score))
+	return output
+		
 
 if __name__ == "__main__":
 	main()
